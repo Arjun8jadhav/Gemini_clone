c1834173ee64349e4c49dec5fe5f58607143ae96
Arjun8jadhav <Jadhav8arjun@gmail.com>
2024-03-26 20:43:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Sidebar from "./compnents/sidebar/sidebar"
+import Main from "./compnents/main/main"
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+      
+    </>
+  )
+}
+
+export default App
